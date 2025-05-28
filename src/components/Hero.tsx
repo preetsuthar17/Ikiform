c729ae4e3ad834089fc8c81a5dfa887000110e11
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-28 10:49:43 +0530

    Add Hero section with title and description for improved user engagement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,16 @@
 const Hero = () => {
   return (
     <>
-      <section></section>
+      <section className="flex items-center justify-center text-center flex-col max-w-6xl w-[95%] mx-auto py-20 gap-5">
+        <h1 className="text-4xl md:text-6xl font-dm-sans font-medium text-center mt-10 flex flex-col gap-3">
+          <span>Beyond just forms.</span>
+          <span>Beautiful forms at your fingertips.</span>
+        </h1>
+        <p>
+          Ikiform is an open-source alternative to Typeform and Google Forms,
+          designed to help you create beautiful forms effortlessly.
+        </p>
+      </section>
     </>
   );
 };

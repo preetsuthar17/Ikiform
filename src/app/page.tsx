0e3b67bebee04e7fdc89a89ddb39872b2abbd91f
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-28 10:49:32 +0530

    Add Hero component to Home page for enhanced user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <div className="font-[family-name:var(--font-inter)]"></div>;
+  return (
+    <div className="font-[family-name:var(--font-inter)]">
+      <Hero />
+    </div>
+  );
 }

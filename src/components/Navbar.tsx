eb713874f35a11a5525f7f29d9374188d70b4a71
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-28 10:49:47 +0530

    Add Navbar component with logo and social links for improved navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,41 @@
 "use client";
 
+import Image from "next/image";
+
 const Navbar = () => {
   return (
     <>
-      <nav></nav>
+      <nav className="flex  justify-between max-w-6xl w-[95%] mx-auto py-10 text-sm font-inter flex-wrap gap-8 max-sm:text-center max-sm:items-center max-sm:justify-center max-sm:flex-col">
+        <div className="max-w-[90px] ">
+          <Image
+            src="/text-logo.svg"
+            alt="Ikiform"
+            width={100}
+            height={100}
+            className="pointer-events-none"
+          />
+        </div>
+        <div>
+          <p className="text-gray-500 font-medium">
+            Star us on{" "}
+            <a
+              href="https://github.com/preetsuthar17/Ikiform"
+              className="underline text-black"
+              target="_blank"
+            >
+              GitHub
+            </a>{" "}
+            and follow us on{" "}
+            <a
+              href="https://x.com/preetsuthar17"
+              className="underline text-black"
+              target="_blank"
+            >
+              X (Twitter)
+            </a>
+          </p>
+        </div>
+      </nav>
     </>
   );
 };
